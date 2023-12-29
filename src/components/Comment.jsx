1da004a8eb6f3bb36f4f027feb6c5e8871f735fe
gabiediasalves <gabiediasalves@gmail.com>
2023-12-29 15:09:40 -0300

    estilizacao-comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/gabiediasalves.png"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabrieli Alves</strong>
+                            <time title="18 de dez às 22:45" dateTime="2023-12-28 22:44:01">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+               <footer>
+                <button>
+                    <ThumbsUp/>
+                    Aplaudir <span> 23</span>
+                </button>
+               </footer>
+            </div>
+        </div>
+    )
+}

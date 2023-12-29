7f257710430959c765bc15454b7d0cdd38932937
gabiediasalves <gabiediasalves@gmail.com>
2023-12-29 16:23:54 -0300

    add-component-avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import {PencilLine} from 'phosphor-react'
 
 import styles from './sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
     return (
@@ -8,7 +9,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1483167496759-08cd7175e235?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         
         <div className={styles.profile}>
-            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/95694730?v=4"/>
+            <Avatar src='https://github.com/gabiediasalves.png'/>
             <strong>Gabrieli Alves</strong>
             <span>Desenvolvedor Plena</span>
         </div>
